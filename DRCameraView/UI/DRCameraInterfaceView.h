bbf058360bfbaf27eb8611583ee010317ee07604
Dal Rupnik <legoless@gmail.com>
2014-04-25 19:46:47 +0200

    Camera interface view added.

---
+++ DRCameraView/UI/DRCameraInterfaceView.h
@@ -0,0 +1,22 @@
+//
+//  DRCameraInterfaceView.h
+//  DRCameraViewSample
+//
+//  Created by Dal Rupnik on 22/04/14.
+//  Copyright (c) 2014 arvystate.net. All rights reserved.
+//
+
+#import "DRCameraView.h"
+
+@interface DRCameraInterfaceView : UIView <DRCameraViewDelegate>
+
+@property (nonatomic, getter = shouldShowPhotoCorners) BOOL showPhotoCorners;
+@property (nonatomic, getter = shouldShowCenterMask) BOOL showCenterMask;
+@property (nonatomic, getter = shouldShowSettingsLabel) BOOL showSettingsLabel;
+
+/*!
+ * Allows some theming of the interface, very basic
+ */
+@property (nonatomic, strong) UIColor* interfaceColor;
+
+@end
